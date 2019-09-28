6db0737217b20d912fa9b9824dd33d2f1c316c04
unknown <pwfcoding@gmail.com>
2019-09-27 23:11:03 -0500

    code working up until I need to update quantity in DB...

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,15 +10,16 @@ let connection = mysql.createConnection({
     database: "bamazon"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadID);
+    console.log("Connected to MySQL DB");
     showProducts();
-    
+    selectItemId();
+
 })
 
 function showProducts() {
-    connection.query("SELECT * FROM products", function(err,res) {
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         for (let i = 0; i < res.length; i++) {
             console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
@@ -26,13 +27,58 @@ function showProducts() {
     })
 }
 
-/*
-function start() {
-    inquirer
-    .prompt({
-        name: "itemid",
-        message: "What ist he ID of the product you would like to purchase?",
-        type: "input"
-    })
+
+function selectItemId() {
+
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+
+        inquirer
+            .prompt([
+                {
+                    name: "choice",
+                    type: "rawlist",
+                    choices: function () {
+                        var choiceArray = [];
+                        for (var i = 0; i < results.length; i++) {
+                            choiceArray.push(results[i].item_id);
+                        }
+                        return choiceArray;
+                    },
+                    message: "What item are you intersted in buying? Please enter the Item ID(number on far left)."
+                },
+                {
+                    name: "purchasequantity",
+                    type: "input",
+                    message: "How many would you like to purchase?"
+                }
+            ])
+            .then(function (answer) {
+                let chosenItem;
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].item_id === answer.choice) {
+                        chosenItem = results[i];
+                    }
+                }
+
+                if (chosenItem.stock_quantity <= parseInt(answer.purchasequantity)) {
+                    console.log("Sorry...we don't have enough in stock.");
+
+                }
+                else {
+                    console.log("Fantastic - we have enough in stock! ENJOY");
+                
+                    connection.query(
+                        "UPDATE products SET ? WHERE ?",
+                        [{
+                            stock_quantity: answer.purchasequantity
+                        }
+
+
+                        ]
+                    )
+
+                }
+            });
+    });
 }
-*/
